6e51f1e5bbb610a7627e4b03b5cb3d1fd296ca70
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2018-10-13 13:26:36 +0200

    #4 adjust CSV export format, so it can be imported to Google Calendar

--- file_impex.py
+++ file_impex.py
@@ -1,8 +1,8 @@
 import csv
-import datetime
 import os
 import time
 from collections import namedtuple
+from datetime import datetime
 
 from icalendar import Calendar, Event
 
@@ -31,13 +31,15 @@ def load_sessions_from_csv(filename='import.csv'):
 
 def convert_csv_row_to_session(headers, row):
     session = namedtuple('Session', ['title', 'id', 'type', 'speakers', 'abstract', 'start', 'end', 'location', 'reserved'])
-    session.title = row[headers.index("title")]
-    session.id = row[headers.index("id")]
+    session.title = row[headers.index("subject")]
+    session.id = row[headers.index("uid")]
     session.type = row[headers.index("type")]
     session.speakers = row[headers.index("speakers")].lstrip('[').rstrip(']').replace("'", '').split(',')
-    session.abstract = row[headers.index("abstract")]
-    session.start = datetime.datetime.strptime(row[headers.index("start")], '%Y-%m-%d %H:%M:%S')
-    session.end = datetime.datetime.strptime(row[headers.index("end")], '%Y-%m-%d %H:%M:%S')
+    session.abstract = row[headers.index("description")]
+    start = row[headers.index("start date")] + ' ' + row[headers.index("start time")]
+    session.start = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
+    end = row[headers.index("end date")] + ' ' + row[headers.index("end time")]
+    session.end = datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
     session.location = row[headers.index("location")]
     session.reserved = row[headers.index("reserved")] == 'True'
     return session
@@ -46,8 +48,11 @@ def convert_csv_row_to_session(headers, row):
 def save_sessions_to_csv(sessions, filename='export.csv'):
     with open(os.path.join(os.path.dirname(__file__), EXPORTS_FOLDER, filename), 'w') as output_file:
         writer = csv.writer(output_file)
-        writer.writerow(('title', 'id', 'type', 'speakers', 'abstract', 'start', 'end', 'location', 'reserved'))  # field header
-        writer.writerows([(session.title, session.id, session.type, session.speakers, session.abstract, session.start, session.end, session.location, session.reserved) for session in sessions])
+        writer.writerow(('subject', 'uid', 'type', 'speakers', 'description', 'start date', 'start time', 'end date', 'end time', 'location', 'reserved'))  # field header
+        writer.writerows([(session.title, session.id, session.type, session.speakers, session.abstract,
+                           datetime.strftime(session.start, '%Y-%m-%d'), datetime.strftime(session.start, '%H:%M:%S'),
+                           datetime.strftime(session.end, '%Y-%m-%d'), datetime.strftime(session.end, '%H:%M:%S'),
+                           session.location, session.reserved) for session in sessions])
 
 
 def save_sessions_to_ical(sessions, filename='export.ics'):
@@ -57,8 +62,8 @@ def save_sessions_to_ical(sessions, filename='export.ics'):
             event = Event()
             event['uid'] = session.id
             event['summary'] = session.title
-            event['dtstart'] = datetime.datetime.strftime(session.start, '%Y%m%dT%H%M%S')
-            event['dtend'] = datetime.datetime.strftime(session.end, '%Y%m%dT%H%M%S')
+            event['dtstart'] = datetime.strftime(session.start, '%Y%m%dT%H%M%S')
+            event['dtend'] = datetime.strftime(session.end, '%Y%m%dT%H%M%S')
             event['description'] = session.abstract
             event['location'] = session.location
             cal.add_component(event)
